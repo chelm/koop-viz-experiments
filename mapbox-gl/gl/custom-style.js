113faf631341f27425ef58cc21c3e1568383dd3a
chelm <christopher.helm@gmail.com>
2014-09-16 11:48:35 -0600

    first cut v tiles

--- mapbox-gl/gl/custom-style.js
+++ mapbox-gl/gl/custom-style.js
@@ -1,6 +1,44 @@
-style.buckets.zipcodes = {
-  "filter": {"source": "zipcodes", "layer": "tile"},
+style.buckets.Counties = {
+  "filter": {"source": "basemap", "layer": "Counties"},
   "fill": true
+};
+
+style.layers.push({
+  "id": "Counties",
+  "bucket": "Counties"
+});
+
+style.styles.default.Counties = {
+  "fill-color": "#039",
+  "fill-opacity": {
+    "fn": "stops",
+    "stops": [[10, .5], [18, .9]]
+  }
+};
+
+style.buckets.land = {
+  "filter": {"source": "basemap", "layer": "Land\\Facility Areas"},
+  "fill": true
+};
+
+style.layers.push({
+  "id": "land",
+  "bucket": "land"
+});
+
+style.styles.default.land = {
+  "fill-color": "#0f5",
+  "fill-opacity": {
+    "fn": "stops",
+    "stops": [[10, .5], [18, .9]]
+  }
+};
+
+
+/*style.buckets.zipcodes = {
+  "filter": {"source": "zipcodes", "layer": "tile"},
+  "fill": true,
+  "line": false
 };
 
 style.layers.push({
@@ -11,9 +49,10 @@ style.layers.push({
 style.styles.default.zipcodes = {
   "fill-color": "#039",
   "stroke-color":"#eee",
+  "line-width": 4,
   "fill-opacity": {
     "fn": "stops",
     "stops": [[4, .9], [10, .5]]
   }
-};
+};*/
 
